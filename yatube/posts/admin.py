c0d00ea5cd793ddc14ab5f8a86c51ef7fc93c291
AlexBulavkin <alexbulavkin@gmail.com>
2022-03-08 01:48:17 +0700

    homework tasks

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+# Из модуля models импортируем модели Post, Group
+from .models import Post, Group
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    # Перечисляем поля, которые должны отображаться в админке
+    list_display = ('pk',
+                    'text',
+                    'pub_date',
+                    'author',
+                    'group',)
+    # Добавляем возможность редактирования поля group
+    list_editable = ('group',)
+    # Добавляем интерфейс для поиска по тексту постов
+    search_fields = ('text',)
+    # Добавляем возможность фильтрации по дате
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
+# При регистрации модели Post источником конфигурации для неё назначаем
+# класс PostAdmin
+admin.site.register(Post, PostAdmin)
+# Регистрируем  модель Group
+admin.site.register(Group)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    template = 'posts/index.html'
+    title = 'Последние обновления на сайте'
+    text = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+        'text': text,
+        'posts': posts,
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Посты группы : {slug}')
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    template = 'posts/group_list.html'
+    title = f'Записи сообщества: {slug}'
+    context = {
+        'title': title,
+        'posts': posts,
+        'group': group,
+    }
+    return render(request, template, context)
